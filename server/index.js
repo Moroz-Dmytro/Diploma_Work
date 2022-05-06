3891c909a8340176ce8579443e7562f49ce1df65
Moroz Dmytro <dmytro101100@gmail.com>
2022-05-06 21:13:54 +0300

    Added User account page

--- server/index.js
+++ server/index.js
@@ -109,7 +109,7 @@ app.post('/login', (req, res) => {
                     if (response) {
                         const id = result[0].id
                         const token = jwt.sign({id}, "jwtSecret", {
-                            expiresIn: 300,
+                            expiresIn: 60 * 60 * 12,
                         })
                         req.session.user = result;
 
